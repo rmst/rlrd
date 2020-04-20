6f178eea6ec5212c4d044b450a378eb3512723b0
yannbouteiller <yann.bouteiller@hotmail.fr>
2020-04-20 15:30:38 -0400

    simple concat MLP

--- agents/sac_models_rd.py
+++ agents/sac_models_rd.py
@@ -98,10 +98,105 @@ class DelayedBranchedMlpModule(Module):
 		return h
 
 
+class DelayedMlpModule(Module):
+	def __init__(self, observation_space, action_space, device, is_Q_network, hidden_units: int = 256, obs_delay=True, act_delay=True):  # FIXME: action_space param is useless
+		"""
+		Args:
+			observation_space:
+				Tuple((
+					obs_space,  # most recent observation
+					Tuple([act_space] * (obs_delay_range.stop + act_delay_range.stop)),  # action buffer
+					Discrete(obs_delay_range.stop),  # observation delay int64
+					Discrete(act_delay_range.stop),  # action delay int64
+				))
+			action_space
+			device: the device on which the variables are instanciated
+			is_Q_network: bool: if True, the input of forward() expects the action to be appended at the end of the input
+			hidden_units: number of output units of this module
+			(optional) obs_delay: bool (default True): if False, the observation delay of observation_space will be ignored (e.g. unknown)
+			(optional) act_delay: bool (default True): if False, the action delay of observation_space will be ignored (e.g. unknown)
+		"""
+		super().__init__()
+		assert isinstance(observation_space, gym.spaces.Tuple)
+		# TODO: check that it is actually an instance of:
+		# Tuple((
+		# 	obs_space,  # most recent observation
+		# 	Tuple([act_space] * (obs_delay_range.stop + act_delay_range.stop)),  # action buffer
+		# 	Discrete(obs_delay_range.stop),  # observation delay int64
+		# 	Discrete(act_delay_range.stop),  # action delay int64
+		# ))
+
+		self.device = device
+		self.is_Q_network = is_Q_network
+		self.act_delay = act_delay
+		self.obs_delay = obs_delay
+
+		self.obs_dim = observation_space[0].shape[0]
+		self.buf_size = len(observation_space[1])
+		self.act_dim = observation_space[1][0].shape[0]
+		assert self.act_dim == action_space.shape[0], f"action spaces mismatch: {self.act_dim} and {action_space.shape[0]}"
+
+		if self.is_Q_network:
+			if self.act_delay and self.obs_delay:
+				self.lin = Linear(self.obs_dim + (self.act_dim + 2) * self.buf_size + self.act_dim, hidden_units)
+			elif self.act_delay or self.obs_delay:
+				self.lin = Linear(self.obs_dim + (self.act_dim + 1) * self.buf_size + self.act_dim, hidden_units)
+			else:
+				self.lin = Linear(self.obs_dim + self.act_dim * self.buf_size + self.act_dim, hidden_units)
+		else:
+			if self.act_delay and self.obs_delay:
+				self.lin = Linear(self.obs_dim + (self.act_dim + 2) * self.buf_size, hidden_units)
+			elif self.act_delay or self.obs_delay:
+				self.lin = Linear(self.obs_dim + (self.act_dim + 1) * self.buf_size, hidden_units)
+			else:
+				self.lin = Linear(self.obs_dim + self.act_dim * self.buf_size, hidden_units)
+
+	def forward(self, x):
+		assert isinstance(x, tuple)
+		# TODO: check that x is actually in:
+		# Tuple((
+		# 	obs_space,  # most recent observation
+		# 	Tuple([act_space] * (obs_delay_range.stop + act_delay_range.stop)),  # action buffer
+		# 	Discrete(obs_delay_range.stop),  # observation delay int64
+		# 	Discrete(act_delay_range.stop),  # action delay int64
+		# ))
+
+		# TODO: double check that everything is correct (dims, devices, autograd)
+
+		# print(f"DEBUG: forward --------------------------")
+		# print(f"DEBUG: is_Q_network:{self.is_Q_network}")
+		# print(f"DEBUG:x[0].shape:{x[0].shape}")
+		# print(f"DEBUG: len(x[1]):{len(x[1])}")
+		# print(f"DEBUG:x[2]:{x[2]}")
+		# print(f"DEBUG:x[3]:{x[3]}")
+
+		obs = x[0]
+		act_buf = torch.cat(x[1], dim=1)
+
+		input = torch.cat((obs, act_buf), dim=1)
+
+		batch_size = obs.shape[0]
+		if self.obs_delay:
+			obs_del = x[2]
+			obs_one_hot = torch.zeros(batch_size, self.buf_size, device=self.device).scatter_(1, obs_del.unsqueeze(1), 1.0)  # TODO: check that scatter_ doesn't create a [1.0] tensor on CPU
+			input = torch.cat((input, obs_one_hot), dim=1)
+		if self.act_delay:
+			act_del = x[3]
+			act_one_hot = torch.zeros(batch_size, self.buf_size, device=self.device).scatter_(1, act_del.unsqueeze(1), 1.0)  # TODO: check that scatter_ doesn't create a [1.0] tensor on CPU
+			input = torch.cat((input, act_one_hot), dim=1)
+		if self.is_Q_network:
+			act = x[4]  # TODO: check that this is correct
+			input = torch.cat((input, act), dim=1)
+
+		h = self.lin(input)
+
+		return h
+
+
 class MlpActionValue(Sequential):
-	def __init__(self, observation_space, action_space, hidden_units, device):
+	def __init__(self, observation_space, action_space, hidden_units, device, act_delay=True, obs_delay=True):
 		super().__init__(
-			DelayedBranchedMlpModule(observation_space, action_space, device, is_Q_network=True), ReLU(),
+			DelayedMlpModule(observation_space, action_space, device, is_Q_network=True, act_delay=act_delay, obs_delay=obs_delay), ReLU(),
 			Linear(hidden_units, hidden_units), ReLU(),
 			Linear(hidden_units, 2)  # reward and entropy predicted separately
 		)
@@ -113,9 +208,9 @@ class MlpActionValue(Sequential):
 
 
 class MlpPolicy(Sequential):
-	def __init__(self, observation_space, action_space, hidden_units, device):
+	def __init__(self, observation_space, action_space, hidden_units, device, act_delay=True, obs_delay=True):
 		super().__init__(
-			DelayedBranchedMlpModule(observation_space, action_space, device, is_Q_network=False), ReLU(),
+			DelayedMlpModule(observation_space, action_space, device, is_Q_network=False, act_delay=act_delay, obs_delay=obs_delay), ReLU(),
 			Linear(hidden_units, hidden_units), ReLU(),
 			TanhNormalLayer(hidden_units, action_space.shape[0])
 		)
@@ -126,11 +221,11 @@ class MlpPolicy(Sequential):
 
 
 class Mlp(ActorModule):
-	def __init__(self, observation_space, action_space, hidden_units: int = 256, num_critics: int = 2):
+	def __init__(self, observation_space, action_space, hidden_units: int = 256, num_critics: int = 2, act_delay=False, obs_delay=False):
 		super().__init__()
 		assert isinstance(observation_space, gym.spaces.Tuple)
-		self.critics = ModuleList(MlpActionValue(observation_space, action_space, hidden_units, self.device) for _ in range(num_critics))
-		self.actor = MlpPolicy(observation_space, action_space, hidden_units, self.device)
+		self.critics = ModuleList(MlpActionValue(observation_space, action_space, hidden_units, self.device, act_delay=act_delay, obs_delay=obs_delay) for _ in range(num_critics))
+		self.actor = MlpPolicy(observation_space, action_space, hidden_units, self.device, act_delay=act_delay, obs_delay=obs_delay)
 		self.critic_output_layers = [c[-1] for c in self.critics]
 
 
